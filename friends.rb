3ecc36190896a1aaa9758c1ef47d0739a703e740
Erik Riggs <riggs.erik@gmail.com>
2022-06-08 15:45:23 -0600

    Testing new methods

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ arrays = ["John", "Kyle", "Jim", "Kevin", "Lenny"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["Hawaii","Japan","Ukraine","Mountains","Carribbean"]
+puts places.shift
